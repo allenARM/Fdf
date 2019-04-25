8f4012c011136082130f30e030767e5716c2a2de
Allen Melikian <allenwem@gmail.com>
2019-04-24 19:54:28 -0700

    normed

--- src/read_file.c
+++ src/read_file.c
@@ -6,11 +6,37 @@
 /*   By: amelikia <amelikia@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/10 12:21:29 by amelikia          #+#    #+#             */
-/*   Updated: 2019/04/24 19:49:12 by amelikia         ###   ########.fr       */
+/*   Updated: 2019/04/24 19:53:53 by amelikia         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fdf.h"
+
+void	print_border_lines(t_fdf *fdf)
+{
+	int i;
+
+	i = 0;
+	while (i < fdf->height - 1)
+	{
+		fdf->first->x = fdf->xyz_modif[i][fdf->width - 1]->x;
+		fdf->first->y = fdf->xyz_modif[i][fdf->width - 1]->y;
+		fdf->second->x = fdf->xyz_modif[i + 1][fdf->width - 1]->x;
+		fdf->second->y = fdf->xyz_modif[i + 1][fdf->width - 1]->y;
+		printline(fdf);
+		i++;
+	}
+	i = 0;
+	while (i < fdf->width - 1)
+	{
+		fdf->first->x = fdf->xyz_modif[fdf->height -1][i]->x;
+		fdf->first->y = fdf->xyz_modif[fdf->height -1][i]->y;
+		fdf->second->x = fdf->xyz_modif[fdf->height -1][i + 1]->x;
+		fdf->second->y = fdf->xyz_modif[fdf->height -1][i + 1]->y;
+		printline(fdf);
+		i++;
+	}
+}
 
 void	print_figure(t_fdf *fdf)
 {
@@ -31,20 +57,11 @@ void	print_figure(t_fdf *fdf)
 			fdf->second->x = fdf->xyz_modif[i][j + 1]->x;
 			fdf->second->y = fdf->xyz_modif[i][j + 1]->y;
 			printline(fdf);
-			fdf->first->x = fdf->xyz_modif[fdf->height -1][j]->x;
-			fdf->first->y = fdf->xyz_modif[fdf->height -1][j]->y;
-			fdf->second->x = fdf->xyz_modif[fdf->height -1][j + 1]->x;
-			fdf->second->y = fdf->xyz_modif[fdf->height -1][j + 1]->y;
-			printline(fdf);
 			j++;
 		}
-		fdf->first->x = fdf->xyz_modif[i][fdf->width - 1]->x;
-		fdf->first->y = fdf->xyz_modif[i][fdf->width - 1]->y;
-		fdf->second->x = fdf->xyz_modif[i + 1][fdf->width - 1]->x;
-		fdf->second->y = fdf->xyz_modif[i + 1][fdf->width - 1]->y;
-		printline(fdf);
 		i++;
 	}
+	print_border_lines(fdf);
 }
 
 void	read_file(t_fdf *fdf, char *str)
